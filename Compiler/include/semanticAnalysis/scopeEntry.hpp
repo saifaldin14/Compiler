e21cf917e34bd88dbd0ff9588d80f0031005cbe0
saifaldin14 <saifaldindeveloper@gmail.com>
2023-03-30 17:49:32 -0400

    Create scopeEntry.hpp

---
+++ Compiler/include/semanticAnalysis/scopeEntry.hpp
@@ -0,0 +1,13 @@
+//
+//  scopeEntry.hpp
+//  Compiler
+//
+//  Created by Saif Al-Din Ali on 2023-03-30.
+//
+
+#ifndef scopeEntry_hpp
+#define scopeEntry_hpp
+
+#include <stdio.h>
+
+#endif /* scopeEntry_hpp */
