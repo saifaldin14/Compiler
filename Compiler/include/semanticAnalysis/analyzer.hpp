c2d6725d5e89026392af6ec773745e2f9718679d
saifaldin14 <saifaldindeveloper@gmail.com>
2023-03-30 16:52:24 -0400

    Create new analyzer file

---
+++ Compiler/include/semanticAnalysis/analyzer.hpp
@@ -0,0 +1,13 @@
+/*
+ File: analyzer.hpp
+ Project: Compiler
+ Author: Saif Al-Din Ali
+ Created: 2023-03-30
+*/
+
+#ifndef analyzer_hpp
+#define analyzer_hpp
+
+#include <stdio.h>
+
+#endif /* analyzer_hpp */
---
+++ Compiler/src/semanticAnalysis/analyzer.cpp
@@ -0,0 +1,8 @@
+/*
+ File: analyzer.cpp
+ Project: Compiler
+ Author: Saif Al-Din Ali
+ Created: 2023-03-30
+*/
+
+#include "../../include/semanticAnalysis/analyzer.hpp"
