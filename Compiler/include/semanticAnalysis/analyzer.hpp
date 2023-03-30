be507d644837049574c88cbb168a40c157abe6f4
saifaldin14 <saifaldindeveloper@gmail.com>
2023-03-30 16:54:32 -0400

    Update analyzer.hpp

--- Compiler/include/semanticAnalysis/analyzer.hpp
+++ Compiler/include/semanticAnalysis/analyzer.hpp
@@ -5,9 +5,31 @@
  Created: 2023-03-30
 */
 
-#ifndef analyzer_hpp
-#define analyzer_hpp
+#ifndef ANALYZER_H
+#define ANALYZER_H
 
-#include <stdio.h>
+#include <iostream>
+#include <vector>
+#include <string>
+#include <sstream>
+#include <cstdlib>
+#include <sstream>
+#include <stdexcept>
+#include <execinfo.h>
+#include <unordered_map>
+#include "../lexicalAnalysis/lexer.hpp"
+#include "syntaxTree.hpp"
+#include "../tokens/token.hpp"
 
+using namespace std;
+
+class Analyzer
+{
+public:
+    Analyzer(vector<vector<Token>> inputLines);
+    
+
+private:
+    vector<vector<Token>> lines;
+};
 #endif /* analyzer_hpp */
