1d3583f26ea423e544892c3a769933568c961a33
saifaldin14 <saifaldindeveloper@gmail.com>
2023-04-01 19:47:35 -0400

    Update scopeVariable.cpp

--- Compiler/src/semanticAnalysis/scopeVariable.cpp
+++ Compiler/src/semanticAnalysis/scopeVariable.cpp
@@ -7,6 +7,16 @@
 
 #include "../../include/semanticAnalysis/scopeVariable.hpp"
 
+/*
+ Description:
+    Turn the scope variable to a readable string
+ 
+ Parameters:
+    - None
+ 
+ Returns:
+    - ret: The values of the scope variable as text (string)
+*/
 string ScopeVariable::toString() {
     string ret;
     
@@ -19,9 +29,29 @@ string ScopeVariable::toString() {
     return ret;
 }
 
+/*
+ Description:
+    Clears the values of the scope variable (but not the scope)
+ 
+ Parameters:
+    - None
+ 
+ Returns:
+    - None
+*/
 void ScopeVariable::setEmptyValues() {
     varName = "";
     type = "";
 }
 
+/*
+ Description:
+    Clears the scope of the scope varibale
+ 
+ Parameters:
+    - None
+ 
+ Returns:
+    - None
+*/
 void ScopeVariable::clearScope() { scope = ""; }
