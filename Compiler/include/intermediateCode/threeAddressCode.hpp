3a1b72c4bc0ed72133c5fa2e066559b82185b5d4
saifaldin14 <saifaldindeveloper@gmail.com>
2023-04-10 14:07:48 -0400

    Create new files

---
+++ Compiler/include/intermediateCode/threeAddressCode.hpp
@@ -0,0 +1,13 @@
+//
+//  threeAddressCode.hpp
+//  Compiler
+//
+//  Created by Saif Al-Din Ali on 2023-04-10.
+//
+
+#ifndef threeAddressCode_hpp
+#define threeAddressCode_hpp
+
+#include <stdio.h>
+
+#endif /* threeAddressCode_hpp */
---
+++ Compiler/src/intermediateCode/threeAddressCode.cpp
@@ -0,0 +1,8 @@
+//
+//  threeAddressCode.cpp
+//  Compiler
+//
+//  Created by Saif Al-Din Ali on 2023-04-10.
+//
+
+#include "../include/intermediateCode/threeAddressCode.hpp"
