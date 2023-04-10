ea620a8deb7e800ae640951ffe47f816196a3187
saifaldin14 <saifaldindeveloper@gmail.com>
2023-04-10 14:32:08 -0400

    Update threeAddressCode.hpp

--- Compiler/include/intermediateCode/threeAddressCode.hpp
+++ Compiler/include/intermediateCode/threeAddressCode.hpp
@@ -18,6 +18,7 @@
 #include <stdexcept>
 #include <execinfo.h>
 #include <unordered_map>
+#include "../tokens/token.hpp"
 
 using namespace std;
 
@@ -27,6 +28,35 @@ public:
     
 
 private:
+    // Used for the Symbol Table (same as Semantic Analysis)
+    inline static const string FUNCTION = "FUNCTION";
+    inline static const string IF = "IF";
+    inline static const string ELSE = "ELSE";
+    inline static const string WHILE = "WHILE";
+    
+    // Reserved key words for IC
+    inline static const string BRANCH = "B";
+    inline static const string BRANCH_LINK = "BL";
+    inline static const string BEGIN = "Begin";
+    inline static const string PUSH = "push";
+    inline static const string POP = "pop";
+    inline static const string LR = "LR";
+    inline static const string FP = "FP";
+    inline static const string PRINT = "print";
+    
+    // Comparator words
+    inline static const string COMPARE = "cmp";
+    inline static const string GREATER_THAN = "bgt";
+    inline static const string GREATER_THAN_EQUAL = "bge";
+    inline static const string LESS_THAN = "blt";
+    inline static const string LESS_THAN_EQUAL = "ble";
+    inline static const string EQUAL = "beq";
+
+    vector<vector<Token>> lines; // Keep track of the code
+    vector<string> scopes; // Stack to keep track of the current scope we are in
+    string printString;
+    
+    
     
 };
 #endif /* threeAddressCode_hpp */
