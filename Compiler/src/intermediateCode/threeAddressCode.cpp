6197dfa029b32c98161c82a6f2925b6e2a023133
saifaldin14 <saifaldindeveloper@gmail.com>
2023-04-10 14:36:57 -0400

    Update threeAddressCode.cpp

--- Compiler/src/intermediateCode/threeAddressCode.cpp
+++ Compiler/src/intermediateCode/threeAddressCode.cpp
@@ -6,3 +6,10 @@
 //
 
 #include "../../include/intermediateCode/threeAddressCode.hpp"
+
+ThreeAddressCode::ThreeAddressCode(vector<vector<Token>> inputLines) {
+    lines = inputLines;
+    scopes.push_back("GLOBAL");
+    printString += "GLOBAL SCOPE";
+    printString += '\n';
+}
