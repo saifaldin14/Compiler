216713a3b4af02b3c30d8ab7ea1a48ba9dace460
saifaldin14 <saifaldindeveloper@gmail.com>
2023-04-15 09:37:44 -0400

    Add comments

--- Compiler/src/intermediateCode/compareToken.cpp
+++ Compiler/src/intermediateCode/compareToken.cpp
@@ -7,15 +7,43 @@
 
 #include "../../include/intermediateCode/compareToken.hpp"
 
+/*
+ Description:
+    Construtor for the Compare Token
+ 
+ Parameters:
+    - inputToken: The default token (Token)
+    - inputPosition: The default position of the token relative to others (int)
+*/
 CompareToken::CompareToken(Token inputToken, int inputPosition) {
     token = inputToken;
     position = inputPosition;
 }
 
+/*
+ Description:
+    Sets the token value
+ 
+ Parameters:
+    - inputToken: The token to set (Token)
+ 
+ Returns:
+    - None
+*/
 void CompareToken::setToken(Token inputToken) {
     token = inputToken;
 }
 
+/*
+ Description:
+    Sets the token position
+ 
+ Parameters:
+    - inputPosition: The position of the token relative to others to set (int)
+ 
+ Returns:
+    - None
+*/
 void CompareToken::setPosition(int inputPosition) {
     position = inputPosition;
 }
--- Compiler/src/intermediateCode/saveToken.cpp
+++ Compiler/src/intermediateCode/saveToken.cpp
@@ -7,15 +7,43 @@
 
 #include "../../include/intermediateCode/saveToken.hpp"
 
+/*
+ Description:
+    Construtor for the Save Token
+ 
+ Parameters:
+    - inputValue: The default values (vector<Token>)
+    - inputPosition: The default position of the token relative to others (int)
+*/
 SaveToken::SaveToken(vector<Token> inputValue, int inputPosition) {
     values = inputValue;
     position = inputPosition;
 }
 
+/*
+ Description:
+    Sets the token value
+ 
+ Parameters:
+    - inputValue: The values to set (vector<Token>)
+ 
+ Returns:
+    - None
+*/
 void SaveToken::setValue(vector<Token> inputValue) {
     values = inputValue;
 }
 
+/*
+ Description:
+    Sets the token position
+ 
+ Parameters:
+    - inputPosition: The position of the token relative to others to set (int)
+ 
+ Returns:
+    - None
+*/
 void SaveToken::setPosition(int inputPosition) {
     position = inputPosition;
 }
