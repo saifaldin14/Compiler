bb44b1160ba020ef30cebdb1e97e3e2128ee0653
saifaldin14 <saifaldindeveloper@gmail.com>
2023-07-05 18:48:10 -0400

    Change file extension

--- Compiler/src/main.cpp
+++ Compiler/src/main.cpp
@@ -10,7 +10,7 @@
 using namespace std;
 
 int main() {
-    FileReader fileReader("../input/test10.cp");
+    FileReader fileReader("../input/test1.ep");
     
     string filename = "../output/error.txt";
     fstream errorFile;
